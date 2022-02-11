65d55b13e15948090a05557c7b17387fa1567268
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2022-02-11 15:41:10 +0300

    create the Express server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const controllerRouting = require('./routes/index');
 
 const app = express();
-const port = process.env.DB_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 controllerRouting(app);
 
